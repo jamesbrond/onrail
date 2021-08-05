b37ec967c1231898b7ef85ced856c02a1c630024
Brondo <diego.brondo@philips.com>
2021-08-05 13:37:56 +0100

    fix: kindly check exception

--- onrail.py
+++ onrail.py
@@ -1,8 +1,15 @@
-from config import Config
-from fare import Fare
 
 def main():
-  fare = Fare()
+  import sys
+  from fare import Fare
+  import traceback
+  try:
+    fare = Fare()
+    sys.exit(0)
+  except Exception as e:
+    print("ERROR: " + str(e), file=sys.stderr)
+    # traceback.print_exc()
+    sys.exit(1)
 
 if __name__ == "__main__":
   main()
